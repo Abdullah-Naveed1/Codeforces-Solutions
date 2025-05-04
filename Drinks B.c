aed5fe51f4d6f99cdfbd81df507ec7b895bb693c
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-04 23:12:35 +0500

    Solved Codeforces: Drinks B.c

---
+++ Drinks B.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//int main()
+//{
+//	long double o;
+//	int n, sum = 0;
+//	scanf("%d", &n);
+//	int a[n];
+//	for (int i = 0;i < n;i++)
+//	{
+//		scanf("%d", &a[i]);
+//		sum += a[i];
+//	}
+//	o = (long double)sum / n;
+//	printf("%Lf", o);
+//	return 0;
+//}
