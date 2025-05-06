a83141907f27d054c3ae6f3161b1a3c6995c0902
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-06 23:18:45 +0500

    Solved Codeforces: Ultra-Fast Mathematician.c

---
+++ Ultra-Fast Mathematician.c
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//int main()
+//{
+//	char s1[101], s2[101];
+//	scanf("%s", s1);
+//	scanf("%s", s2);
+//	for (int i = 0;s1[i] != '\0';i++)
+//	{
+//		if((s1[i]=='1' && s2[i]=='0')||(s1[i]=='0'&&s2[i]=='1'))
+//			printf("1");
+//		else 
+//			printf("0");
+//	}
+//}
