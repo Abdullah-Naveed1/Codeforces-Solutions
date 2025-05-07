d5e5b84a994279466796aa538de7b28a1373a3d8
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-07 19:06:58 +0500

    Solved Codeforces: Divisibility Problem.c

---
+++ Divisibility Problem.c
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int n,num1,num2;
+//	scanf("%d", &n);
+//	while (n--)
+//	{
+//		int count = 0;
+//		scanf("%d%d", &num1, &num2);
+//		if(num1%num2==0)
+//			printf("0\n");
+//		else
+//		{
+//			if(num2>num1)
+//				printf("%d\n", num2 - num1);
+//			else
+//			{
+//				int i = num1 / num2;
+//				int j = num1 % num2;
+//				printf("%d\n", ((i + 1) * num2) - num1);
+//			}
+//		}
+//	}
+//	return 0;
+//}
