0ac3584e95080bd3c3df6581192609724b91889e
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-07 13:07:02 +0500

    Solved Codeforces: Is your horseshoe on the other hoof.c

---
+++ Is your horseshoe on the other hoof.c
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int n1, n2, n3, n4,count=0;
+//	scanf("%d%d%d%d", &n1, &n2, &n3, &n4);
+//	if (n1 == n2 || n1 == n3 || n1 == n4)
+//		count++;
+//	if (n2 == n3 || n2 == n4)
+//		count++;
+//	if(n3==n4)
+//		count++;
+//	printf("%d", count);
+//}
