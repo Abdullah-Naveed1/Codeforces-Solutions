239357040ff6537b08aefcd29536a8afade78b6b
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-12 18:03:00 +0500

    Solved Codeforces: Expression.c

---
+++ Expression.c
@@ -0,0 +1,20 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c,arr[6], max;
+//	scanf("%d %d %d", &a, &b, &c);
+//	arr[0] = a + b + c;
+//	arr[1] = a + (b * c);
+//	arr[2] = a * (b + c);
+//	arr[3] = (a + b) * c;
+//	arr[4] = (a * b) + c;
+//	arr[5] = a * b * c;
+//	max = arr[0];
+//	for (int i = 1;i < 6;i++)
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
