91b4235c2db99ae58ab3b3d5da634f520b48e21a
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-12 20:30:49 +0500

    Solved Codeforces: Games.c

---
+++ Games.c
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int n,games=0,count=0;
+//	scanf("%d", &n);
+//	games = n * (n - 1);
+//	int a[games/2],h[games/2];
+//	for (int i = 0;i <n;i++)
+//	{
+//		scanf("%d", &h[i]);
+//		scanf("%d", &a[i]);
+//	}
+//	for (int i = 0;i < n;i++)
+//	{
+//		for (int j = 0;j <n;j++)
+//		{
+//			if (h[i] == a[j])
+//				count++;
+//		}
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
