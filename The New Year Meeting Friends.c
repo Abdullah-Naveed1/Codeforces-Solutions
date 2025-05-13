b11b06b469e3362487cc487c0a572e97485cb1cb
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-13 22:00:56 +0500

    Solved Codeforces: The New Year Meeting Friends.c

---
+++ The New Year Meeting Friends.c
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int x[3], dis = 0;
+//	scanf("%d %d %d", &x[0], &x[1], &x[2]);
+//	for (int i = 0;i < 3;i++)
+//	{
+//		for (int j = i + 1;j < 3;j++)
+//		{
+//			if (x[i] > x[j])
+//			{
+//				int temp = x[i];
+//				x[i] = x[j];
+//				x[j] = temp;
+//			}
+//		}
+//	}
+//	dis += x[1] - x[0];
+//	dis += x[2] - x[1];
+//	printf("%d", dis);
+//	return 0;
+//}
