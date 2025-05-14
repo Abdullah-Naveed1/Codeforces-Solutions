244f53f31e99e4e9ec164b62cf0d624660f59b05
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-14 16:28:11 +0500

    Solved Codeforces: I_love_username.c

---
+++ I_love_username.c
@@ -0,0 +1,24 @@
+//#include<stdio.h>
+//int main() {
+//	int n,amazing=0,max=0,min=0;
+//	scanf("%d", &n);
+//	int a[n];
+//	for (int i = 0;i < n;i++) {
+//		scanf("%d", &a[i]);
+//	}
+//	max = a[0];
+//	min = a[0];
+//	for (int i = 1;i < n;i++)
+//	{
+//		if (a[i] > max) {
+//			max = a[i];
+//			amazing++;
+//		}
+//		if (a[i] < min) {
+//			min = a[i];
+//			amazing++;
+//		}
+//	}
+//	printf("%d", amazing);
+//	return 0;
+//}
