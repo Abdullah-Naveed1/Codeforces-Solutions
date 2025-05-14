695fd2332f9e10a07fed276372bdf7e55b473ae2
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-14 19:03:50 +0500

    Solved Codeforces: Buy a Shovel.c

---
+++ Buy a Shovel.c
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//int main() {
+//	int k, r,shovel=0,sentinel=1,money;
+//	scanf("%d %d", &k, &r);
+//	for (int i = 1; i <= 10; ++i) {
+//		int total = i * k;
+//		if (total % 10 == 0 || total % 10 == r) {
+//			printf("%d\n", i);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
