4ce5fba1c4f6a534fd4032cb544cf3758e8008f8
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-14 21:01:57 +0500

    Solved Codeforces: Vasya the Hipster.c

---
+++ Vasya the Hipster.c
@@ -0,0 +1,26 @@
+//#include<stdio.h>
+//int main() {
+//	int red, blue,days_diff=0,days_same=0;
+//	scanf("%d %d", &red, &blue);
+//	while (red && blue) {
+//		red--;
+//		blue--;
+//		days_diff++;
+//	}
+//	if (red == 0)
+//	{
+//		if (blue>=2)
+//		{
+//			days_same = blue / 2;
+//		}
+//	}
+//	else if (blue == 0)
+//	{
+//		if (red >= 2)
+//		{
+//			days_same = red / 2;
+//		}
+//	}
+//	printf("%d %d", days_diff,days_same);
+//
+//}
