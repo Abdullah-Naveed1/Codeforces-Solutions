35fa2aa6a5fd57360a18dc5f1d5efef4ed8a53fd
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-15 19:15:34 +0500

    Solved Codeforces: Division.c

---
+++ Division.c
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		int n;
+//		scanf("%d", &n);
+//		if (n >= 1900) {
+//			puts("Division 1");
+//		}
+//		else if (n >= 1600 && n <= 1899)
+//		{
+//			puts("Division 2");
+//		}
+//		else if (n >= 1400 && n <= 1599)
+//		{
+//			puts("Division 3");
+//		}
+//		else {
+//			puts("Division 4");
+//		}
+//	}
+//}
