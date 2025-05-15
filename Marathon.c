6b55e45643b2c8a723d4654cd72d6eba977352b7
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-15 19:07:13 +0500

    Solved Codeforces: Marathon.c

---
+++ Marathon.c
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		int count = 0;
+//		int a, b, c, d;
+//		scanf("%d %d %d %d", &a, &b, &c, &d);
+//		if (b>a)
+//			count++;
+//		if (c>a)
+//			count++;
+//		if (d>a)
+//			count++;
+//		printf("%d\n", count);
+//	}
+//	return 0;
+//}
