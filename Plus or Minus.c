1f2a62d52154af00c3db656610dc255e0c8de793
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-16 09:59:19 +0500

    Solved Codeforces: Plus or Minus.c

---
+++ Plus or Minus.c
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		int a, b, c;
+//		scanf("%d %d %d", &a, &b, &c);
+//		if (a + b == c)
+//			puts("+");
+//		else if (a - b == c)
+//			puts("-");
+//	}
+//	return 0;
+//}
