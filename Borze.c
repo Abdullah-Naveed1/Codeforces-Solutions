c891bf39cb068ae187db6cd049b79372bae90af7
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-18 16:18:40 +0500

    Solved Codeforces: Borze.c

---
+++ Borze.c
@@ -0,0 +1,20 @@
+//#include<stdio.h>
+//int main() {
+//	char s[201];
+//	scanf("%s", s);
+//	for (int i = 0;s[i] != '\0';i++) {
+//		if (s[i] == '.')
+//		{
+//			printf("0");
+//		}
+//		else if (s[i] == '-' && s[i + 1] == '.') {
+//			printf("1");
+//			i++;
+//		}
+//		else if (s[i] == '-' && s[i] == '-') {
+//			printf("2");
+//			i++;
+//		}
+//	}
+//	return 0;
+//}
