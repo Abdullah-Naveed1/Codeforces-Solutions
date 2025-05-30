5f391e5d53d442915c5af1c18fa29a84354b9d4c
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-05-30 19:04:11 +0500

    Solved Codeforces: IQ Test.c

---
+++ IQ Test.c
@@ -0,0 +1,22 @@
+//#include<stdio.h>
+//int main() {
+//	int n,count_e=0,count_o=0,index_e,index_o;
+//	scanf("%d", &n);
+//	int a[n];
+//	for (int i = 0;i < n;i++) {
+//		scanf("%d", &a[i]);
+//		if (a[i] % 2 == 0) {
+//			count_e++;
+//			index_e = i;
+//		}
+//		else {
+//			count_o++;
+//			index_o = i;
+//		}
+//	}
+//	if (count_e == 1)
+//		printf("%d", index_e + 1);
+//	else
+//		printf("%d", index_o + 1);
+//	return 0;
+//}
