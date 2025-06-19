b674a477f0d664f47e4015c5626df47e9c723983
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-06-19 22:27:36 +0500

    Solved Codeforces: Holiday of Equality.c

---
+++ Holiday of Equality.c
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//int main() {
+//	int n;
+//	scanf("%d", &n);
+//	int a[n];
+//	int max=0,count=0;
+//	for (int i = 0;i < n;i++) {
+//		scanf("%d", &a[i]);
+//		if (a[i] > max)
+//			max = a[i];
+//	}
+//	for (int i = 0;i < n;i++) {
+//		count += max - a[i];
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
