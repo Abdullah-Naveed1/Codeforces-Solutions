e250add35a8e9a58d31b97e7ef5185f804035dd3
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-06-25 17:17:26 +0500

    Solved Codeforces: Design Tutorial Learn from Math.c

---
+++ Design Tutorial Learn from Math.c
@@ -0,0 +1,20 @@
+//#include<stdio.h>
+//#include<math.h>
+//int isComposite(int n) {
+//	for (int i = 2;i <= sqrt(n);i++) {
+//		if (n % i == 0)
+//			return 1;
+//	}
+//	return 0;
+//}
+//int main() {
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 1;i <= n;i++) {
+//		if (isComposite(i) && isComposite(n - i)) {
+//			printf("%d %d", i, n - i);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
