9abc6808e175b8a3118039bfa9ad7278d5efaf10
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-06-25 17:07:53 +0500

    Solved Codeforces: Dislike of Threes.c

---
+++ Dislike of Threes.c
@@ -0,0 +1,22 @@
+//#include<stdio.h>
+//int polycarp(int a) {
+//	int i=1,count=0;
+//	for (;1;) {
+//		if (i % 3 != 0 && i % 10 != 3) {
+//			count++;
+//			if (count == a)
+//				return i;
+//		}
+//		i++;
+//	}
+//}
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		int k;
+//		scanf("%d", &k);
+//		int result = polycarp(k);
+//		printf("%d\n",result);
+//	}
+//}
