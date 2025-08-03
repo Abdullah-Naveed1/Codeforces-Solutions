c6057e9eced0a58ffc4e4742f5fec33c8845d0bb
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-08-03 14:16:32 +0500

    Solved Codeforces: Choosing Teams.c

---
+++ Choosing Teams.c
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//int main() {
+//	int n, k,count=0;
+//	scanf("%d %d", &n, &k);
+//	int a[n];
+//	for (int i = 0; i < n; i++) {
+//		scanf("%d", &a[i]);
+//		if (a[i] + k <= 5)
+//			count++;
+//	}
+//	printf("%d", count / 3);
+//	return 0;
+//}
