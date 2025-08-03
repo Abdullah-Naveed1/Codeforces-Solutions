688ddb786758294784857b56f69d207be409e3b6
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-08-03 13:38:52 +0500

    Solved Codeforces: Game with Integers.c

---
+++ Game with Integers.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//
+//int main() {
+//    int t;
+//    scanf("%d", &t);
+//    while (t--) {
+//        int n, vanya = 0;
+//        scanf("%d", &n);
+//        if (n % 3 == 0)
+//            printf("Second\n");
+//        else
+//            printf("First\n");
+//    }
+//    return 0;
+//}
