877ed9f72845bc84344167f80cb8790a837d8a7e
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-08-21 22:19:51 +0500

    Solved Codeforces: Creating Words.c

---
+++ Creating Words.c
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		char a[4], b[4];
+//		scanf(" %3s", a);
+//		scanf(" %3s", b);
+//		printf("%c%c%c %c%c%c\n", b[0], a[1], a[2], a[0], b[1], b[2]);
+//	}
+//	return 0;
+//}
