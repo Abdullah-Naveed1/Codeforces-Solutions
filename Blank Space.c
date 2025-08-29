4404dbc6ebbd0dbede804983b6e99edaf648732b
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-08-29 15:05:48 +0500

    Solved Codeforces: Blank Space.c

---
+++ Blank Space.c
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		int n,max=0,count=0;
+//		scanf("%d", &n);
+//		int a[n];
+//		for (int i = 0; i < n; i++) {
+//			scanf("%d", &a[i]);
+//		}
+//		for (int i = 0; i < n; i++) {
+//			if (a[i] == 0) {
+//				count++;
+//				if(count>max)
+//					max = count;
+//			}
+//			else {
+//				count = 0;
+//			}
+//		}
+//		printf("%d\n", max);
+//	}
+//	return 0;
+//}
