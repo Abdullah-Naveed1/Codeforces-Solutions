974d24735d35ee1abd931a8d2ef4e20adac68be6
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-03 21:04:27 +0500

    Solved Codeforces: Array Coloring.c

---
+++ Array Coloring.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		int n,countOdd=0;
+//		scanf("%d", &n);
+//		int a[n];
+//		for (int i = 0; i < n; i++) {
+//			scanf("%d", &a[i]);
+//			if (a[i] % 2 == 1)
+//				countOdd++;
+//		}
+//		if (countOdd % 2 == 1)
+//			puts("NO");
+//		else
+//			puts("YES");
+//	}
+//}
