95a3a18b4518ef6576a7aa195a495e0b3d76295d
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-04 12:12:53 +0500

    Solved Codeforces: Minutes Before the New Year.c

---
+++ Minutes Before the New Year.c
@@ -0,0 +1,11 @@
+//#include<stdio.h>
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		int h, m;
+//		scanf("%d%d", &h, &m);
+//		int minutes = (h * 60) + m;
+//		printf("%d\n", 1440 - minutes);
+//	}
+//}
