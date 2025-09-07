2b6043853f3e2b51b1a8facecc745388f9316ad5
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-07 13:30:06 +0500

    Solved Codeforces: Minimal Square.c

---
+++ Minimal Square.c
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//int Max(int num1, int num2) {
+//    if (num1 > num2)
+//        return num1;
+//    else
+//        return num2;
+//}
+//int main() {
+//    int t;
+//    scanf("%d", &t);
+//    while (t--) {
+//        int a, b;
+//        scanf("%d %d", &a, &b);
+//        if (a < b) {
+//            int max = Max(2 * a, b);
+//            printf("%d\n", max * max);
+//        }
+//        else {
+//            int max = Max(a, 2 * b);
+//            printf("%d\n", max * max);
+//        }
+//    }
+//    return 0;
+//}
