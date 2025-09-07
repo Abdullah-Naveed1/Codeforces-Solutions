b8adb916b10647a222bb7aca22457c105946f7d0
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-07 18:22:28 +0500

    Solved Codeforces: Fair Playoff.c

---
+++ Fair Playoff.c
@@ -0,0 +1,35 @@
+//#include<stdio.h>
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+//		int a[4],max=0,max2nd=0;
+//		for (int i = 0; i < 4; i++) {
+//			scanf("%d", &a[i]);
+//			if (a[i] > max) {
+//				max2nd = max;
+//				max = a[i];
+//			}
+//			else if (a[i] > max2nd) {
+//				max2nd = a[i];
+//			}
+//		}
+//		int finalist1, finalist2;
+//		if (a[0] > a[1]) {
+//			finalist1 = a[0];
+//		}
+//		else {
+//			finalist1 = a[1];
+//		}
+//		if (a[2] > a[3]) {
+//			finalist2 = a[2];
+//		}
+//		else {
+//			finalist2 = a[3];
+//		}
+//		if ((finalist1 == max && finalist2 == max2nd) || (finalist1 == max2nd && finalist2 == max))
+//			puts("YES");
+//		else
+//			puts("NO");
+//	}
+//}
