5c0a84fc60bf21aaf0d86886b8286066351d15f1
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-08 17:17:41 +0500

    Solved Codeforces: Bachgold Problem.cpp

---
+++ Bachgold Problem.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int n,count;
+//	cin >> n;
+//	if (n % 2 == 0) {
+//		count = n / 2;
+//		cout << count << endl;
+//		for (int i = 1; i <= count; i++) {
+//			cout << 2<<" ";
+//		}
+//		cout << endl;
+//	}
+//	else {
+//		int div = (n - 3) / 2;
+//		count = div + 1;
+//		cout << count << endl;
+//		for (int i = 1; i <= div; i++) {
+//			cout << 2 << " ";
+//		}
+//		cout << 3 << endl;
+//	}
+//	return 0;
+//}
