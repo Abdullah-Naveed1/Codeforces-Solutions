084a5cbea33f9485f61393eddd0b90386b92756e
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-12 18:39:56 +0500

    Solved Codeforces: Triple.cpp

---
+++ Triple.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//
+//int main() {
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		int n;
+//		cin >> n;
+//		int a[n];
+//		for (int i = 0; i < n; i++) {
+//			cin >> a[i];
+//		}
+//		sort(a, a + n);
+//		int output=-1;
+//		for (int i = 0; i < n; i++) {
+//			if ((i + 1 < n && i + 2 < n) && a[i] == a[i + 1] && a[i + 1] == a[i + 2]) {
+//				output = a[i];
+//				break;
+//			}
+//		}
+//		cout << output << endl;
+//	}
+//}
