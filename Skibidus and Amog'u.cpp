b547919c19d2f826ea7ceacc91bd6372817ab918
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-16 20:02:01 +0500

    Solved Codeforces: Skibidus and Amog'u.cpp

---
+++ Skibidus and Amog'u.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//void fastIO() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//}
+//int main() {
+//	fastIO();
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		string word;
+//		cin >> word;
+//		int len = word.length();
+//		for (int i = 0; i < len - 2; i++) {
+//			cout << word[i];
+//		}
+//		cout << 'i' << endl;
+//	}
+//	return 0;
+//}
