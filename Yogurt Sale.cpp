b476c46fdf9fad44d57a904fdaf60b2504e0ce1f
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-20 16:48:54 +0500

    Solved Codeforces: Yogurt Sale.cpp

---
+++ Yogurt Sale.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int n, a, b,count=0;
+//	cin >> n >> a >> b;
+//	float bSingle = (float)b / 2;
+//	if (bSingle < a) {
+//		if (n % 2 == 0) {
+//			count += b * (n / 2);
+//		}
+//		else {
+//			count += b * (n - 1) / 2;
+//			count += a;
+//		}
+//	}
+//	else {
+//		count = a * n;
+//	}
+//	cout << count << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
