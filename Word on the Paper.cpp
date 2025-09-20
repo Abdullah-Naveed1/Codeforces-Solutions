d8123da7c3731e5e091c698d3f75463315d011d3
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-20 16:55:43 +0500

    Solved Codeforces: Word on the Paper.cpp

---
+++ Word on the Paper.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//void solve() {
+//	vector<string> board(8);
+//	for (int i = 0; i < 8; i++) {
+//		cin >> board[i];
+//	}
+//	for (int i = 0; i < 8; i++) {
+//		for (int j = 0; j < 8; j++) {
+//			if (board[i][j] >= 'a' && board[i][j] <= 'z') {
+//				cout << board[i][j];
+//			}
+//		}
+//	}
+//	cout << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
