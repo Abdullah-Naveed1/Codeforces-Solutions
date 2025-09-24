dd5e7d87d407700a7ba6328c7b14bb63dffba77e
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-24 18:16:36 +0500

    Solved Codeforces: Trippi Troppi.cpp

---
+++ Trippi Troppi.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	string s1, s2, s3;
+//	cin >> s1 >> s2 >> s3;
+//	cout << s1[0] << s2[0] << s3[0]<<endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
