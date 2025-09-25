c7329cee0a3f45528fc79efc5ae30141b76b433e
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-25 21:50:28 +0500

    Solved Codeforces: Array with Odd Sum.cpp

---
+++ Array with Odd Sum.cpp
@@ -0,0 +1,41 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//void solve() {
+//	int n;
+//	cin >> n;
+//	vector<int> vec;
+//	int countOdd = 0,countEven=0;
+//	for (int i = 0; i < n; i++) {
+//		int a;
+//		cin >> a;
+//		vec.push_back(a);
+//		if (a % 2 == 1) {
+//			countOdd++;
+//		}
+//		else {
+//			countEven++;
+//		}
+//	}
+//	if (countEven==n) {
+//		cout << "NO" << endl;
+//	}
+//	else if (countOdd % 2 == 1) {
+//		cout << "YES" << endl;
+//	}
+//	else if(countOdd%2==0&&countEven>0){
+//		cout << "YES" << endl;
+//	}
+//	else {
+//		cout << "NO" << endl;
+//	}
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
