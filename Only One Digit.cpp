cda1853f7b346bce7eab420625852a904fd6cd04
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-27 17:17:30 +0500

    Solved Codeforces: Only One Digit.cpp

---
+++ Only One Digit.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int x,min=10;
+//	cin >> x;
+//	while (x!=0) {
+//		int dig = (x % 10);
+//		if (dig < min) {
+//			min = dig;
+//			
+//		}
+//		x /= 10;
+//	}
+//	cout << min << endl;
+//	return;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
