fe1075702f345c4d4ba0a4647e2e6db222d02470
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-28 18:24:39 +0500

    Solved Codeforces: C+=.cpp

---
+++ C+=.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int a, b, n,count=0;
+//	cin >> a >> b >> n;
+//	while (a <= n && b<=n) {
+//		if (a > b)
+//			b += a;
+//		else
+//			a += b;
+//		count++;
+//	}
+//	cout << count << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
