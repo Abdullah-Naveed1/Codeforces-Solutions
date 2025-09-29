39197806c4ba0a59f79981f4b01eb9db58f9fc81
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-09-29 17:08:05 +0500

    Solved Codeforces: Coins.cpp

---
+++ Coins.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	long long n, k;
+//	cin >> n >> k;
+//	if (k > n) {
+//		cout << "NO" << endl;
+//		return;
+//	}
+//	if (n % 2 == 0) {
+//		cout << "YES" << endl;
+//		return;
+//	}
+//	else {
+//		if ((n - k)%2 == 0) {
+//			cout << "YES" << endl;
+//		}
+//		else {
+//			cout << "NO" << endl;
+//		}
+//	}
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
