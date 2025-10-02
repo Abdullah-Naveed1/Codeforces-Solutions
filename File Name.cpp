b8b3d584b73f513433f3979ea0bcbde9021e42d7
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-02 17:18:07 +0500

    Solved Codeforces: File Name.cpp

---
+++ File Name.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int n,count=0;
+//	cin >> n;
+//	string name;
+//	cin >> name;
+//	for (int i = 0; i < n; i++) {
+//		if (name[i] == 'x' && name[i + 1] == 'x' && name[i+2] == 'x') {
+//			count++;
+//		}
+//	}
+//	cout << count << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	int t=1;
+//	//cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
