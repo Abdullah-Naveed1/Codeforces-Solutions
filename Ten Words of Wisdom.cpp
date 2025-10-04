a4ea193671f94119d9ab004501dcd3c84b7b1250
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-04 17:58:56 +0500

    Solved Codeforces: Ten Words of Wisdom.cpp

---
+++ Ten Words of Wisdom.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int n,quality=0,max=0,count=0;
+//	cin >> n;
+//	int a[n], b[n];
+//	for (int i = 0; i < n; i++) {
+//		cin >> a[i];
+//		cin >> b[i];
+//		if (a[i] <= 10) {
+//			count++;
+//			if (b[i] > max) {
+//				max = b[i];
+//				quality = i;
+//			}
+//		}
+//	}
+//	if (count == 0) {
+//		cout << 0 << endl;
+//	}
+//	else
+//		cout << quality + 1 << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
