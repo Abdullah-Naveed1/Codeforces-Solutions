89d076171f4849c756435d02147571158c256dc2
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-06 12:41:52 +0500

    Solved Codeforces: Problem Generator.cpp

---
+++ Problem Generator.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int n, m;
+//	cin >> n >> m;
+//	string s;
+//	cin >> s;
+//	int count[7] = { 0 };
+//	for (int i = 0; i < n; i++) {
+//		count[s[i] - 'A']++;
+//	}
+//	int finalCount = 0;
+//	for (int i = 0; i < 7; i++) {
+//		if (count[i] < m) {
+//			finalCount += m - count[i];
+//		}
+//	}
+//	cout << finalCount << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
