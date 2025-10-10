3eef48d795e9c68e77b61af35089398c7af9baa4
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-10 17:46:17 +0500

    Solved Codeforces: Quintomania.cpp

---
+++ Quintomania.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int n;
+//	cin >> n;
+//	int a[n];
+//	for (int i = 0; i < n; i++) {
+//		cin >> a[i];
+//	}
+//	for (int i = 0; i < n-1; i++) {
+//		if (abs(a[i] - a[i + 1]) != 5 && abs(a[i] - a[i + 1]) != 7) {
+//			cout << "NO" << endl;
+//			return;
+//		}
+//	}
+//	cout << "YES" << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
