b9710be037ffe45fabe6dacb6d7d2baa60462231
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-19 13:16:56 +0500

    Solved Codeforces: Rook.cpp

---
+++ Rook.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	string pos;
+//	cin >> pos;
+//	for (int i = 0; i < 8; i++) {
+//		if (i+1 != pos[1]-'0') {
+//			cout << pos[0] << i+1 << endl;
+//		}
+//	}
+//	for (int i = 0; i < 8; i++) {
+//		if (i != pos[0] - 'a') {
+//			cout << static_cast<char>(i + 'a') << pos[1]<<endl;
+//		}
+//	}
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
