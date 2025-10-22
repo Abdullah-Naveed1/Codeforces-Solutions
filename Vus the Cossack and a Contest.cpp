7a51215897d2bae0b4640120d084556cee6da057
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-22 23:58:11 +0500

    Solved Codeforces: Vus the Cossack and a Contest.cpp

---
+++ Vus the Cossack and a Contest.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int n, m, k;
+//	cin >> n >> m >> k;
+//	if (m >= n && k >= n) cout << "YES" << endl;
+//	else cout << "NO" << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t=1;
+//	//cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
