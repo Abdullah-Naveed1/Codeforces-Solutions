fd2ad7d06e3c923e57eadffa97fc62b73565268f
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-24 21:29:23 +0500

    Solved Codeforces: Goals of Victory.cpp

---
+++ Goals of Victory.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int n;
+//	cin >> n;
+//	int a[n - 1],sum=0;
+//	for (int i = 0; i < n - 1; i++) {
+//		cin >> a[i];
+//		sum += a[i];
+//	}
+//	cout << -sum << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
