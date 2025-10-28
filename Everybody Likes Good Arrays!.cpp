0346db9d44ac11bdeb60c71aa56355ee52c1b6b8
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-28 17:07:19 +0500

    Solved Codeforces: Everybody Likes Good Arrays!.cpp

---
+++ Everybody Likes Good Arrays!.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//void solve() {
+//	int n;
+//	cin >> n;
+//	int a[n];
+//	for (int i = 0; i < n; i++) {
+//		cin >> a[i];
+//	}
+//	int count = 0;
+//	for (int i = 0; i < n - 1; i++) {
+//		if (a[i] % 2 == a[i + 1] % 2)	count++;
+//	}
+//	cout << count << endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
