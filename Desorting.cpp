e5b7f2627a86bf41c5cbbf8f520f6068ca5a94e3
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2025-10-31 14:06:12 +0500

    Solved Codeforces: Desorting.cpp

---
+++ Desorting.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//bool notSorted(int a[], int size) {
+//	for (int i = 0; i < size-1; i++) {
+//		if (a[i] > a[i + 1])
+//			return true;
+//	}
+//	return false;
+//}
+//void solve() {
+//	int n;
+//	cin >> n;
+//	int a[n];
+//	for (int i = 0; i < n; i++) {
+//		cin >> a[i];
+//	}
+//	if (notSorted(a, n)) { cout << 0 << endl; return; }
+//	int smallestDiff = INT_MAX;
+//	for (int i = 0; i < n - 1; i++) {
+//		if (abs(a[i] - a[i + 1]) < smallestDiff)
+//			smallestDiff = abs(a[i] - a[i + 1]);
+//	}
+//	cout << smallestDiff / 2+1<<endl;
+//}
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		solve();
+//	}
+//}
