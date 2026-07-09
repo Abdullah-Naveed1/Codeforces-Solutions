748df1f5aee7b99d2cc56a877001482539bf6c8f
Abdullah Naveed <abdullahnaveedhero@gmail.com>
2026-07-09 12:16:23 +0500

    Solved Codeforces: New Year's Number.cpp

---
+++ New Year's Number.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void solve(){
+	
+}
+void printYes(){
+    cout<<"Yes"<<endl;
+}
+void printNo(){
+    cout<<"No"<<endl;
+}
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+}
